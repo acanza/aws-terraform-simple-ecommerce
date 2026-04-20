3c1cd90fa0e4ad14c6949c2b27cc1e2d956d9c2e
Antonio Cantero <antonio@theclassyoga.com>
2026-04-20 11:15:34 +0200

    Replacied all WP references with Medusa Commercein Terraform files

--- envs/dev/locals.tf
+++ envs/dev/locals.tf
@@ -2,7 +2,7 @@
 locals {
   # Determine RDS endpoint
   # Use the actual RDS module output (EC2 will be created after RDS)
-  db_host = var.wordpress_db_host != "" ? var.wordpress_db_host : module.rds[0].db_instance_address
+  db_host = var.medusa_db_host != "" ? var.medusa_db_host : module.rds[0].db_instance_address
 
   # Medusa initialization script - read and replace placeholders
   # We use a simple replacement approach instead of templatefile to avoid conflicts with bash variables
@@ -14,12 +14,12 @@ locals {
             file("${path.module}/medusa-init.sh"),
             "%%DB_HOST%%", local.db_host
           ),
-          "%%DB_NAME%%", var.wordpress_database_name
+          "%%DB_NAME%%", var.medusa_database_name
         ),
         "%%DB_USER%%", "postgres"
       ),
       "%%DB_PASSWORD%%", var.rds_master_password
     ),
-    "%%ADMIN_USER%%", var.wordpress_admin_user
+    "%%ADMIN_USER%%", var.medusa_admin_user
   ))
 }
